89cee08c6175d5ca9d9e1fa1692e74140117f90a
saltkid <ojisaltkid@gmail.com>
2024-01-17 18:20:21 +0800

    forgot to stage main lul

--- main.go
+++ main.go
@@ -4,7 +4,6 @@ import (
 	"fmt"
 	"log"
 	"os"
-	"path/filepath"
 )
 
 func main() {
@@ -16,59 +15,6 @@ func main() {
 	// if cmd.IsNone() {
 	rename()
 	// }
-}
-
-type Command struct {
-	name         string
-	values       []string
-	isValidValue func(string) error
-}
-
-// define commands here
-var CLI_CMDS = []Command{ADD_CMD}
-var (
-	ADD_CMD = Command{
-		name: "add",
-		isValidValue: func(path string) error {
-			// check if exists
-			if _, err := os.Stat(path); os.IsNotExist(err) {
-				return fmt.Errorf("%s does not exist: %s", path, err.Error())
-			}
-
-			// check if has any image files
-			imgFileCount := 0
-			err := filepath.WalkDir(path, func(p string, d os.DirEntry, err error) error {
-				if err != nil {
-					return err
-				}
-
-				if !d.IsDir() && d.Name() != filepath.Base(path) && IsImageFile(d.Name()) {
-					imgFileCount++
-				}
-
-				return nil
-			})
-
-			if err != nil {
-				return fmt.Errorf("error reading %s: %s", path, err.Error())
-			}
-
-			if imgFileCount < 1 {
-				return fmt.Errorf("no image files in %s", path)
-			}
-
-			return nil
-		},
-	}
-)
-
-func IsValidArgName(a string) bool {
-	for _, ARG := range CLI_CMDS {
-		if a != ARG.name {
-			return false
-		}
-	}
-	return true
 }
 
 func ParseArgs(rawArgs []string) (Command, error) {
