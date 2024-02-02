f2c8d1e82121310b904779a73b3fb7700b4eb507
saltkid <ojisaltkid@gmail.com>
2024-02-03 00:01:12 +0800

    cmd/utils.go: helper funcs to extract flags/subcmds from maps

---
+++ cmd/utils.go
@@ -0,0 +1,31 @@
+package cmd
+
+import (
+	"path/filepath"
+	"strings"
+)
+
+func isImageFile(f string) bool {
+	f = strings.ToLower(filepath.Ext(f))
+	return f == ".png" ||
+		f == ".jpg" ||
+		f == ".jpeg"
+}
+
+// returns empty string if not found in map of flags
+func ExtractFlagValue(val flag.FlagType, flags map[flag.FlagType]flag.Flag) string {
+	flag, ok := flags[val]
+	if !ok {
+		return ""
+	}
+	return flag.Value
+}
+
+// returns empty string if not found in map of subcommands
+func ExtractSubCmdValue(val CmdType, subCmds map[CmdType]Cmd) string {
+	subCmd, ok := subCmds[val]
+	if !ok {
+		return ""
+	}
+	return subCmd.Value
+}
