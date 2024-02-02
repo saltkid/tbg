068d318ba76aa6f9ae8a979f3178b316b6f1b492
saltkid <ojisaltkid@gmail.com>
2024-02-02 22:46:53 +0800

    flag value validations

--- flag/flag.go
+++ flag/flag.go
@@ -1,6 +1,10 @@
 package flag
 
-import "fmt"
+import (
+	"fmt"
+	"strconv"
+	"strings"
+)
 
 type FlagType uint8
 
@@ -37,16 +41,72 @@ func ToFlag(s string) (*Flag, error) {
 func (f *Flag) ValidateValue(val string) error {
 	switch f.Type {
 	case Profile:
-		return nil
+		return ValidateProfile(val)
 	case Interval:
-		return nil
+		return ValidateInterval(val)
 	case Alignment:
-		return nil
+		return ValidateAlignment(val)
 	case Opacity:
-		return nil
+		return ValidateOpacity(val)
 	case Stretch:
-		return nil
+		return ValidateStretch(val)
 	default:
 		return fmt.Errorf("unexpected error: unknown flag: %d", f.Type)
+	}
+}
+
+// putting flags value validation in here for now
+func ValidateProfile(val string) error {
+	if val == "default" {
+		return nil
+	}
+	list, num, isList := strings.Cut(val, "-")
+	if list != "list" {
+		return fmt.Errorf("invalid arg '%s' for --profile: must be 'list' followed by a dash then number", val)
+	}
+	if !isList {
+		return fmt.Errorf("invalid arg '%s' for --profile: list and number must be separated by '-'", val)
+	}
+	_, err := strconv.Atoi(num)
+	if err != nil {
+		return fmt.Errorf("invalid arg '%s' for --profile: %s", val, err.Error())
+	}
+	return nil
+}
+
+func ValidateInterval(val string) error {
+	_, err := strconv.Atoi(val)
+	if err != nil {
+		return fmt.Errorf("invalid arg '%s' for --interval: %s", val, err.Error())
+	}
+	return nil
+}
+
+func ValidateAlignment(val string) error {
+	switch val {
+	case "top-right", "tr", "top-left", "tl", "top", "t", "left", "l", "center", "c", "right", "r", "bottom-right", "br", "bottom-left", "bl", "bottom", "b":
+		return nil
+	default:
+		return fmt.Errorf("invalid arg '%s' for --alignment: unknown alignment", val)
+	}
+}
+
+func ValidateOpacity(val string) error {
+	num, err := strconv.ParseFloat(val, 64)
+	if err != nil {
+		return fmt.Errorf("invalid arg '%s' for --opacity: %s", val, err.Error())
+	}
+	if num < 0 || num > 1 {
+		return fmt.Errorf("invalid arg '%s' for --opacity: must be between 0 and 1", val)
+	}
+	return nil
+}
+
+func ValidateStretch(val string) error {
+	switch val {
+	case "none", "fill", "uniform", "uniform-fill":
+		return nil
+	default:
+		return fmt.Errorf("invalid arg '%s' for --stretch: unknown stretch", val)
 	}
 }
