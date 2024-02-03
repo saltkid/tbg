468c92c32417201a10427524272b48429f75e099
saltkid <ojisaltkid@gmail.com>
2024-02-03 23:24:36 +0800

    run command code structure

---
+++ cmd/run.go
@@ -0,0 +1,25 @@
+package cmd
+
+import (
+	"github.com/saltkid/tbg/flag"
+)
+
+func RunValidateValue(val string) error {
+	// TODO
+	return nil
+}
+
+func RunValidateFlag(f *flag.Flag) error {
+	// TODO
+	return nil
+}
+
+func RunValidateSubCmd(sc *Cmd) error {
+	// TODO
+	return nil
+}
+
+func RunExecute(c *Cmd) error {
+	// TODO
+	return nil
+}
