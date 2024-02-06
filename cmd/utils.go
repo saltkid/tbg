da3bac75b0293a102b8642839b25335332b3fce9
saltkid <ojisaltkid@gmail.com>
2024-02-06 16:48:32 +0800

    helper func to extract flag/subcmd type

--- cmd/utils.go
+++ cmd/utils.go
@@ -4,10 +4,28 @@ import (
 	"github.com/saltkid/tbg/flag"
 )
 
-// returns empty string if not found in map of flags
-func ExtractFlagValue(val flag.FlagType, flags map[flag.FlagType]*flag.Flag) string {
+// returns nil if not found in map of flags
+func ExtractFlag(val flag.FlagType, flags map[flag.FlagType]*flag.Flag) *flag.Flag {
 	flag, ok := flags[val]
 	if !ok {
+		return nil
+	}
+	return flag
+}
+
+// returns nil if not found in map of subcommands
+func ExtractSubCmd(val CmdType, subCmds map[CmdType]*Cmd) *Cmd {
+	subCmd, ok := subCmds[val]
+	if !ok {
+		return nil
+	}
+	return subCmd
+}
+
+// returns empty string if not found in map of flags
+func ExtractFlagValue(val flag.FlagType, flags map[flag.FlagType]*flag.Flag) string {
+	flag := ExtractFlag(val, flags)
+	if flag == nil {
 		return ""
 	}
 	return flag.Value
@@ -15,8 +33,8 @@ func ExtractFlagValue(val flag.FlagType, flags map[flag.FlagType]*flag.Flag) str
 
 // returns empty string if not found in map of subcommands
 func ExtractSubCmdValue(val CmdType, subCmds map[CmdType]*Cmd) string {
-	subCmd, ok := subCmds[val]
-	if !ok {
+	subCmd := ExtractSubCmd(val, subCmds)
+	if subCmd == nil {
 		return ""
 	}
 	return subCmd.Value
