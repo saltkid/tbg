0acc83a145b719faba38e2568dddfe4641e73a9f
saltkid <ojisaltkid@gmail.com>
2024-10-13 23:21:42 +0800

    feat(flag): flag validators are in a separate file + return validated values
    Flag value is now a *string to indicate that it might be nil (optional)
    
    Reasoning for flag validators returning a validated value is because
    commands can use those validated values and assign them to their struct
    fields for example

--- flag.go
+++ flag.go
@@ -2,37 +2,35 @@ package main
 
 import (
 	"fmt"
-	"strconv"
-	"strings"
 )
 
 type FlagType uint8
 
 const (
-	None FlagType = iota
-	Profile
-	Interval
-	Alignment
-	Opacity
-	Stretch
-	Random
+	NoFlag FlagType = iota
+	ProfileFlag
+	IntervalFlag
+	AlignmentFlag
+	OpacityFlag
+	StretchFlag
+	RandomFlag
 )
 
-func (f FlagType) ToString() string {
+func (f FlagType) String() string {
 	switch f {
-	case None:
+	case NoFlag:
 		return "none"
-	case Profile:
+	case ProfileFlag:
 		return "--profile"
-	case Interval:
+	case IntervalFlag:
 		return "--interval"
-	case Alignment:
+	case AlignmentFlag:
 		return "--alignment"
-	case Opacity:
+	case OpacityFlag:
 		return "--opacity"
-	case Stretch:
+	case StretchFlag:
 		return "--stretch"
-	case Random:
+	case RandomFlag:
 		return "--random"
 	default:
 		return "unknown"
@@ -41,117 +39,51 @@ func (f FlagType) ToString() string {
 
 type Flag struct {
 	Type  FlagType
-	Value string
+	Value *string
 }
 
 func ToFlag(s string) (*Flag, error) {
 	switch s {
 	case "--profile", "-p":
-		return &Flag{Type: Profile}, nil
+		return &Flag{Type: ProfileFlag}, nil
 	case "--interval", "-i":
-		return &Flag{Type: Interval}, nil
+		return &Flag{Type: IntervalFlag}, nil
 	case "--alignment", "-a":
-		return &Flag{Type: Alignment}, nil
+		return &Flag{Type: AlignmentFlag}, nil
 	case "--opacity", "-o":
-		return &Flag{Type: Opacity}, nil
+		return &Flag{Type: OpacityFlag}, nil
 	case "--stretch", "-s":
-		return &Flag{Type: Stretch}, nil
+		return &Flag{Type: StretchFlag}, nil
 	case "--random", "-r":
-		return &Flag{Type: Random}, nil
+		return &Flag{Type: RandomFlag}, nil
 	default:
 		return nil, fmt.Errorf("unknown flag: %s", s)
 	}
 }
 
-func (f *Flag) IsNone() bool {
-	return f.Type == None
-}
-
-func (f *Flag) ValidateValue(val string) error {
+func (f *Flag) ValidateValue(val *string) error {
 	switch f.Type {
-	case Profile:
-		return ValidateProfile(val)
-	case Interval:
-		return ValidateInterval(val)
-	case Alignment:
-		return ValidateAlignment(val)
-	case Opacity:
-		return ValidateOpacity(val)
-	case Stretch:
-		return ValidateStretch(val)
-	case Random:
-		return ValidateRandom(val)
-	case None:
+	case ProfileFlag:
+		_, err := ValidateProfile(val)
+		return err
+	case IntervalFlag:
+		_, err := ValidateInterval(val)
+		return err
+	case AlignmentFlag:
+		_, err := ValidateAlignment(val)
+		return err
+	case OpacityFlag:
+		_, err := ValidateOpacity(val)
+		return err
+	case StretchFlag:
+		_, err := ValidateStretch(val)
+		return err
+	case RandomFlag:
+		_, err := ValidateRandom(val)
+		return err
+	case NoFlag:
 		return nil
 	default:
 		return fmt.Errorf("unexpected error: unknown flag: %d", f.Type)
-	}
-}
-
-// putting flags value validation in here for now
-func ValidateProfile(val string) error {
-	if val == "default" {
-		return nil
-	}
-	list, num, isList := strings.Cut(val, "-")
-	if list != "list" {
-		return fmt.Errorf("invalid arg '%s' for --profile: must be 'list' followed by a dash then number", val)
-	}
-	if !isList {
-		return fmt.Errorf("invalid arg '%s' for --profile: list and number must be separated by '-'", val)
-	}
-	_, err := strconv.Atoi(num)
-	if err != nil {
-		return fmt.Errorf("invalid arg '%s' for --profile: %s", val, err.Error())
-	}
-	return nil
-}
-
-func ValidateInterval(val string) error {
-	num, err := strconv.Atoi(val)
-	if err != nil {
-		return fmt.Errorf("invalid arg '%s' for --interval: %s", val, err.Error())
-	}
-	if num < 1 {
-		return fmt.Errorf("invalid arg '%s' for --interval: must be greater than 0", val)
-	}
-	return nil
-}
-
-func ValidateAlignment(val string) error {
-	switch val {
-	case "topLeft", "top", "topRight", "left", "center", "right", "bottomLeft", "bottom", "bottomRight":
-		return nil
-	default:
-		return fmt.Errorf("invalid arg '%s' for --alignment: unknown alignment", val)
-	}
-}
-
-func ValidateOpacity(val string) error {
-	num, err := strconv.ParseFloat(val, 64)
-	if err != nil {
-		return fmt.Errorf("invalid arg '%s' for --opacity: %s", val, err.Error())
-	}
-	if num < 0 || num > 1 {
-		return fmt.Errorf("invalid arg '%s' for --opacity: must be between 0 and 1", val)
-	}
-	return nil
-}
-
-func ValidateStretch(val string) error {
-	switch val {
-	case "none", "fill", "uniform", "uniformToFill":
-		return nil
-	default:
-		return fmt.Errorf("invalid arg '%s' for --stretch: unknown stretch", val)
-	}
-}
-
-func ValidateRandom(val string) error {
-	switch val {
-	case "":
-		return nil
-	default:
-		return fmt.Errorf("'--random' flag does not take any arguments. got '%s'", val)
 	}
 }
