70e53d14956ca9b281385b80a125515c6826c123
saltkid <ojisaltkid@gmail.com>
2024-10-13 23:57:41 +0800

    feat(command): new run command struct
    TODO: change how changing the background works. the current
    implementation uses one big for loop with a nested for loop inside a
    nested for loop. it is convoluted.
    
    idea: use a struct to store the state and use events (channels) mapped
    to methods to change image (or do some other action)

--- command_run.go
+++ command_run.go
@@ -5,40 +5,101 @@ import (
 	"os"
 )
 
-func RunValidateValue(val string) error {
-	if val == "" {
-		return nil
-	}
-	return fmt.Errorf("'run' takes no arguments. got: '%s'", val)
+type RunCommand struct {
+	Profile   *string
+	Interval  *uint16
+	Alignment *string
+	Stretch   *string
+	Opacity   *float32
+	Random    *bool
 }
 
-func RunValidateFlag(f *Flag) error {
+func (cmd *RunCommand) Type() CommandType { return RunCommandType }
+
+func (cmd *RunCommand) Debug() {
+	fmt.Println("Run Command")
+	fmt.Println("Flags:")
+	if cmd.Profile != nil {
+		fmt.Println(" ", ProfileFlag, *cmd.Profile)
+	}
+	if cmd.Interval != nil {
+		fmt.Println(" ", IntervalFlag, *cmd.Interval)
+	}
+	if cmd.Alignment != nil {
+		fmt.Println(" ", AlignmentFlag, *cmd.Alignment)
+	}
+	if cmd.Stretch != nil {
+		fmt.Println(" ", StretchFlag, *cmd.Stretch)
+	}
+	if cmd.Opacity != nil {
+		fmt.Println(" ", OpacityFlag, *cmd.Opacity)
+	}
+	if cmd.Random != nil {
+		fmt.Println(" ", RandomFlag, *cmd.Random)
+	}
+}
+
+func (cmd *RunCommand) ValidateValue(val *string) error {
+	if val == nil || *val == "" {
+		return nil
+	}
+	return fmt.Errorf("'run' takes no arguments. got: '%s'", *val)
+}
+
+func (cmd *RunCommand) ValidateFlag(f Flag) error {
 	switch f.Type {
-	case Profile, Interval, Alignment, Opacity, Stretch, Random:
-		return f.ValidateValue(f.Value)
+	case ProfileFlag:
+		val, err := ValidateProfile(f.Value)
+		if err != nil {
+			return err
+		}
+		cmd.Profile = val
+	case IntervalFlag:
+		val, err := ValidateInterval(f.Value)
+		if err != nil {
+			return err
+		}
+		cmd.Interval = val
+	case AlignmentFlag:
+		val, err := ValidateAlignment(f.Value)
+		if err != nil {
+			return err
+		}
+		cmd.Alignment = val
+	case OpacityFlag:
+		val, err := ValidateOpacity(f.Value)
+		if err != nil {
+			return err
+		}
+		cmd.Opacity = val
+	case StretchFlag:
+		val, err := ValidateStretch(f.Value)
+		if err != nil {
+			return err
+		}
+		cmd.Stretch = val
+	case RandomFlag:
+		val, err := ValidateRandom(f.Value)
+		if err != nil {
+			return err
+		}
+		cmd.Random = val
 	default:
-		return fmt.Errorf("invalid flag for 'run': '%s'", f.Type.ToString())
+		return fmt.Errorf("invalid flag for 'run': '%s'", f.Type)
 	}
+	return nil
 }
 
-func RunValidateSubCmd(sc *Cmd) error {
-	switch sc.Type {
-	case None:
+func (cmd *RunCommand) ValidateSubCommand(sc Command) error {
+	switch sc.Type() {
+	case NoCommandType:
 		return nil
 	default:
-		return fmt.Errorf("'run' takes no sub commands. got: '%s'", sc.Type.ToString())
+		return fmt.Errorf("'run' takes no sub commands. got: '%s'", sc.Type())
 	}
 }
 
-func RunExecute(c *Cmd) error {
-	// get flags if set by user
-	profile := ExtractFlagValue(Profile, c.Flags)
-	interval := ExtractFlagValue(Interval, c.Flags)
-	alignment := ExtractFlagValue(Alignment, c.Flags)
-	opacity := ExtractFlagValue(Opacity, c.Flags)
-	stretch := ExtractFlagValue(Stretch, c.Flags)
-	random := ExtractFlagValue(Random, c.Flags)
-
+func (cmd *RunCommand) Execute() error {
 	configPath, err := ConfigPath()
 	if err != nil {
 		return err
@@ -47,15 +108,23 @@ func RunExecute(c *Cmd) error {
 	if err != nil {
 		return fmt.Errorf("Failed to read config file %s: %s", configPath, err)
 	}
-	configContents := &Config{}
-	err = configContents.Unmarshal(yamlFile)
+	config := new(Config)
+	err = config.Unmarshal(yamlFile)
 	if err != nil {
 		return err
 	}
-
 	err = configContents.ChangeBgImage(configPath, profile, interval, alignment, stretch, opacity, random)
 	if err != nil {
 		return err
 	}
 	return nil
+}
+
+func (config *Config) determineExecutionFlags(cmd *RunCommand) bool {
+	config.Profile = Option(cmd.Profile).UnwrapOr(config.Profile)
+	config.Interval = Option(cmd.Interval).UnwrapOr(config.Interval)
+	config.Alignment = Option(cmd.Alignment).UnwrapOr(config.Alignment)
+	config.Stretch = Option(cmd.Stretch).UnwrapOr(config.Stretch)
+	config.Opacity = Option(cmd.Opacity).UnwrapOr(config.Opacity)
+	return Option(cmd.Random).UnwrapOr(false)
 }
