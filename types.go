dac88eb280d226a33d99113cd0d63b8dc64e71a9
saltkid <ojisaltkid@gmail.com>
2024-10-14 00:07:27 +0800

    feat(types): flesh out the Optional type used in the new Config
    NOTE: as said in Option function's doc comment, use sparingly as it is
    not supported by the stdlib. This is only for convenience where
    one-liners make sense like assigning to a struct field

---
+++ types.go
@@ -0,0 +1,46 @@
+package main
+
+// convenient wrapper around *T to check against nil,
+// choosing between two values based on existence, etc.
+type Optional[T any] struct {
+	val *T
+}
+
+// convenient wrapper around *T to check against nil,
+// choosing between two values based on existence, etc.
+//
+// NOTE: use sparingly as it is not supported by the stdlib
+// and you'd likely have to use *T everywhere else anyway.
+//
+// only use for the convenience methods where oneliners make
+// more sense to use (e.g. assigning to struct fields)
+func Option[T any](val *T) Optional[T] {
+	return Optional[T]{
+		val: val,
+	}
+}
+
+// returns the underlying pointer
+func (o Optional[T]) Pointer() *T {
+	return o.val
+}
+
+// returns the value of the option (pointer) if it is not a nil pointer
+//
+// otherwise just returns the given value
+func (o Optional[T]) UnwrapOr(val T) T {
+	if o.val != nil {
+		return *o.val
+	}
+	return val
+}
+
+// returns the option (pointer) if it is not a nil pointer
+//
+// otherwise just returns the given pointer as an Option
+func (o Optional[T]) Or(val *T) Optional[T] {
+	if o.val != nil {
+		return o
+	}
+	return Option(val)
+}
