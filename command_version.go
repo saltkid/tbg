89ad665ffc6e723f7f648d41c2a8808abf196705
saltkid <ojisaltkid@gmail.com>
2024-10-13 23:48:51 +0800

    feat(command): new version command with value, flag, and sub command validators
    old behavior: acceps any value, flag, and subcommand
    
    now it errors when a any of the three are given. otherwise it still just
    prints out the version

--- command_version.go
+++ command_version.go
@@ -6,7 +6,26 @@ import (
 
 var TbgVersion = "dev"
 
-func VersionExecute() error {
+type VersionCommand struct{}
+
+func (cmd *VersionCommand) Type() CommandType { return VersionCommandType }
+func (cmd *VersionCommand) Debug() {
+	fmt.Println("Version Command")
+	fmt.Println("version:", TbgVersion)
+}
+func (cmd *VersionCommand) ValidateValue(val *string) error {
+	if val == nil || *val == "" {
+		return nil
+	}
+	return fmt.Errorf("'version' takes no args. got: '%s'", *val)
+}
+func (cmd *VersionCommand) ValidateFlag(f Flag) error {
+	return fmt.Errorf("'version' takes no flags. got: '%s'", f.Type)
+}
+func (cmd *VersionCommand) ValidateSubCommand(sc Command) error {
+	return fmt.Errorf("'version' takes no sub-commands. got: '%s'", sc.Type())
+}
+func (cmd *VersionCommand) Execute() error {
 	fmt.Println(TbgVersion)
 	return nil
 }
